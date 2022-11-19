f47b5770d8f3d5d8167cb0e938daf756f4e0c6b2
pasly0920 <inwoo920621@gmail.com>
2022-11-19 13:50:06 +0900

    test: 스프링 컨테이너는 기본적으로 싱글톤

--- src/test/java/hello/core/Singleton/SingletonTest.java
+++ src/test/java/hello/core/Singleton/SingletonTest.java
@@ -5,6 +5,7 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -17,5 +18,27 @@ public class SingletonTest {
         MemberService memberService2 = appConfig.memberService();
 
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        AppConfig appConfig = new AppConfig();
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+                AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
