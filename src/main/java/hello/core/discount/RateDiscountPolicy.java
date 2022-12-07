7c77099637ad23604173ecf38ce00d55cd9e2cee
pasly0920 <inwoo920621@gmail.com>
2022-12-08 01:22:42 +0900

    fix: bean 등록 -> component scan 방식으로

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,15 +2,19 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
+
     @Override
     public int discount(Member member, int price) {
-        if(member.getGrade() == Grade.VIP)
+        if (member.getGrade() == Grade.VIP) {
             return price * discountPercent / 100;
-        else
+        } else {
             return 0;
+        }
     }
 }
